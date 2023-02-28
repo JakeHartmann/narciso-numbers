562c734b0a396d5b0204631764a5b50594470da0
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 21:08:27 -0300

    changed text color depending on result

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function getInputValue(){
     let inputNumber = document.getElementById("number").value;
     inputNum = inputNumber;
     checkNarciso(inputNum);
-    document.getElementById("form").reset();
+    // document.getElementById("form").reset();
 }
 
 // Treats number as an array and displays all of its indexes
@@ -19,9 +19,23 @@ function checkNarciso(numberToBeChecked){
     console.log(list);
     const initValue = 0;
     let cubeNumber = list.reduce((sum, num) => sum + Math.pow(num, 3), initValue);
-    console.log(sexo);
+    console.log(cubeNumber);
 
-    let result = cubeNumber == inputNum ? "Is Narciso" : "Not Narciso";
+    let result = cubeNumber == inputNum ? "Is a Narciso number 👍" : "Isn't a Narciso number ❌";
+    let isNarciso = cubeNumber == inputNum ? true : false;
     console.log(result);
+    console.log(isNarciso);
+
+    if(isNarciso){
+        document.getElementById("result").innerHTML = "Is a Narciso number 👍";
+    document.getElementById("result").classList.remove("incorrect");
+    document.getElementById("result").classList.add("correct");
+    }
+    // document.getElementById("result").textContent = result;
+    else{
+        document.getElementById("result").innerHTML = "Isn't a Narciso number ❌";
+    document.getElementById("result").classList.remove("correct");
+    document.getElementById("result").classList.add("incorrect");
+    }
 
    }
